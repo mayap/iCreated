7fcc7282b089277ffd586f443902d75e3390c7a0
mayap <maya.ppetrova@gmail.com>
2019-01-04 15:02:55 +0200

    Small changes in css

--- api/app.js
+++ api/app.js
@@ -1,10 +1,10 @@
-let express = require('express'),
+const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome!');
-})
+});
 
 app.listen(port, function () {
   console.log('RESTful API server started on: ' + port);
