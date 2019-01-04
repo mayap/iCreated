ebe2f65c5c3b4720bd86bc160ec1d65817bfcb01
mayap <maya.ppetrova@gmail.com>
2019-01-04 12:44:23 +0200

    Initial commit

---
+++ api/app.js
@@ -0,0 +1,11 @@
+let express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Welcome!');
+})
+
+app.listen(port, function () {
+  console.log('RESTful API server started on: ' + port);
+});
---
+++ api/routes/iCreatedRoute.js
@@ -0,0 +1,15 @@
+'use strict';
+export function route(app) {
+  var iCreated = require('../controllers/iCreatedController');
+
+  // iCreated Routes
+  app.route('/tasks')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+};
