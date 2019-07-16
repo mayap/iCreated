80b8fc176a2ea15dc99ad82dff0c3895dca2ceef
mayap <maya.ppetrova@gmail.com>
2019-07-16 10:50:32 +0300

    Upgrade ember version to 3.11

--- iCreated-public/tests/integration/components/category-summary/component-test.js
+++ iCreated-public/tests/integration/components/category-summary/component-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | category-summary', function(hooks) {
 
     await render(hbs`{{category-summary}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | category-summary', function(hooks) {
       {{/category-summary}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
--- iCreated-public/tests/integration/components/search-component/component-test.js
+++ iCreated-public/tests/integration/components/search-component/component-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | search-component', function(hooks) {
 
     await render(hbs`{{search-component}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | search-component', function(hooks) {
       {{/search-component}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
